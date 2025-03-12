ac3bc0158fe902cbb5457e88c240e302d5f09769
yashp0906 <patelyashj9698@gmail.com>
2025-03-12 15:19:41 -0400

    Add dimensions is zero Validation to calculateCanvasSize

--- src/calculateCanvasSize/calculateCanvasSize.test.ts
+++ src/calculateCanvasSize/calculateCanvasSize.test.ts
@@ -4,4 +4,10 @@ describe("calculateCanvasSize", () => {
     // 10 * 100 = 1000
     expect(calculateCanvasSize("10", "100")).toEqual(1000);
   });
+  test("should return 0 when one of the dimensions is zero", () => {
+    // 0 * 100 = 0
+    expect(calculateCanvasSize("0", "100")).toEqual(0);
+    // 100 * 0 = 0
+    expect(calculateCanvasSize("100", "0")).toEqual(0);
+  });
 });
