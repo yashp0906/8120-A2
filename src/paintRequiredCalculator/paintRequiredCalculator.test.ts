aebe568f8100a791b9f2257fa8b3bd9d1e20d892
yashp0906 <patelyashj9698@gmail.com>
2025-03-12 15:28:00 -0400

    Add Validation for 0 input in printRequiredCalculator

--- src/paintRequiredCalculator/paintRequiredCalculator.test.ts
+++ src/paintRequiredCalculator/paintRequiredCalculator.test.ts
@@ -1,9 +1,8 @@
 import paintRequiredCalculator from "./paintRequiredCalculator";
 
 describe("paint required calculator", () => {
-  test("should return expected result", () => {
-    const result = paintRequiredCalculator(50, 10);
-
-    expect(result).toEqual(5);
+  test("should return 0 if the area to be painted is 0", () => {
+    // If no area to paint, no paint should be required
+    expect(paintRequiredCalculator(0, 10)).toEqual(0);
   });
 });
